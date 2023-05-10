c6662968566aa5b5a3bdc732a6b45b4fa71ba764
matteovcc <matteovcc99@gmail.com>
2023-05-10 09:39:51 +0200

    first commit

---
+++ src/lezione/Playground.java
@@ -0,0 +1,14 @@
+package lezione;
+
+public class Playground {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Java è un linguaggio multi-threaded consente di sfruttare il meccanismo della concorrenza logica, 
+//		Parti separate di un programma possono essere eseguite come se fossero processate parallelamente,potendo però accedere a spazi
+//		di indirizzamento della memoria condivisi in modo da poter scambiare in modo semplice le info necessario al corretto funzionamento 
+//		dell'app. Consente ad esempio di eseguire delle operazioni in background senza contstringere l'utente ad attendere il completamento
+//		di tali operazione e continuare ad usare il programma
+	}
+
+}
